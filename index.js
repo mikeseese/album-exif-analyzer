742059f36b7b97181ed0e809a127393f0fd2e450
Mike Seese <seesemichaelj@gmail.com>
2019-07-01 22:21:57 -0700

    get stuff working

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+#!/usr/bin/env node
+
 const exif = require("fast-exif");
 const readdirp = require("readdirp");
 const fs = require("fs");
 
-const filestream = readdirp(".", {
+const filestream = readdirp(process.cwd(), {
   fileFilter: ["*.png", "*.jpg", "*.jpeg", "*.cr2"],
   depth: 50,
 });
@@ -10,31 +12,53 @@ const filestream = readdirp(".", {
 let gotHeader = false;
 const separator = "|";
 
-const outputFile = "output.csv";
+const outputFile = `${Date.now()}-album-exif-analyzer.csv`;
 let header = "";
 let exifData;
+let numProcessing = 0;
 
-filestream.on("end", () => {
-  console.log(header);
-  console.log(exifData);
-  fs.writeFileSync(outputFile, header);
-  let s = "";
-  for (let i = 0; i < exifData[0].length; i++) {
-    for (let j = 0; j < exifData.length; j++) {
-      if (j > 0) {
-        s += separator;
-      }
-      s += exifData[j][i].toString();
-    }
-    s += "\n";
+filestream.on("end", async () => {
+  while (numProcessing > 0) {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 100);
+    });
   }
-  fs.appendFileSync(outputFile, s);
+
+  if (header && exifData.length > 0) {
+    fs.writeFileSync(outputFile, header);
+    let s = "";
+    for (let i = 0; i < exifData[0].length; i++) {
+      for (let j = 0; j < exifData.length; j++) {
+        if (j > 0) {
+          s += separator;
+        }
+        if (Buffer.isBuffer(exifData[j][i])) {
+          s += exifData[j][i].toString("hex");
+        } else {
+          s += exifData[j][i].toString();
+        }
+      }
+      s += "\n";
+    }
+    fs.appendFileSync(outputFile, s);
+    console.log(`Wrote data to ${outputFile}`);
+  } else {
+    console.log("No data found");
+  }
   process.exit();
 });
 
 filestream.on("data", async (entry) => {
-  const data = await exif.read(entry.path);
+  numProcessing++;
+  let data;
+  try {
+    data = await exif.read(entry.path);
+  } catch (e) {
+    numProcessing--;
+    return;
+  }
   if (data === null) {
+    numProcessing--;
     return;
   }
 
@@ -43,7 +67,6 @@ filestream.on("data", async (entry) => {
   if (!gotHeader) {
     gotHeader = true;
     header = exifKeys.join(separator) + "\n";
-    console.log("here");
     exifData = Array(exifKeys.length);
     for (let i = 0; i < exifKeys.length; i++) {
       exifData[i] = [];
@@ -53,4 +76,5 @@ filestream.on("data", async (entry) => {
   for (let i = 0; i < exifKeys.length; i++) {
     exifData[i].push(data.exif[exifKeys[i]]);
   }
+  numProcessing--;
 });
